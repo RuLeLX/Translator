0b8ae1e70ef7cc7ed9ed5fc8a20b27c29346bc10
unknown <sanyapridava@mail.ru>
2024-03-11 20:07:36 +0300

    Recreate base of leksem

--- C#_JS_Translator/C#_JS_Translator/C#_JS_Translator.cpp
+++ C#_JS_Translator/C#_JS_Translator/C#_JS_Translator.cpp
@@ -110,61 +110,25 @@ int main()
     setlocale(LC_ALL, "ru");
 
     //Формируем базу служебных слов С# 
-    ifstream f("SystemWord.txt");
-    string line;
-    string SystemWordBase = "";
-
-    if (f.is_open())
-    {
-
-        while (getline(f, line))
-        {
-            SystemWordBase += line + ' ';
-        }
-
-
-        f.close();
-    }
+    
 
 
     //Формируем базу операций(арифметические, логические, побитовые, унарные, реляционные)
-    ifstream g("Operations.txt");
-    string Operations = "";
-
-    if (g.is_open()) {
-
-        while (getline(g, line)) {
-            Operations += line + ' ';
-        }
-
-
-        g.close();
-    }
+    
 
 
     //Формируем базу разделителей
-    ifstream h("Separators.txt");
-    string Separators = "";
-
-    if (h.is_open()) {
-
-        while (getline(h, line)) {
-            Separators += line + ' ';
-        }
-
-        h.close();
-
-    }
+    
 
 
     //Формируем базу идентификитаров
-    string identificators = "";
+    
 
     //Формируем базу констант
-    string constants = "";
+    
 
     //Внутренне представление лексем
-    string result = "";
+   
 
 
 
